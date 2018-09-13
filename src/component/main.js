9010b447ce2cf3d8d8db4df8cdf8cb520efd7e6c
fangwei <fangwei@aek56.com>
2018-09-13 13:18:23 +0800

    组件只从props中取值，可以改用纯函数组件了 存在问题: 从store的state中获取的内容不能被自定义

--- src/component/main.js
+++ src/component/main.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import connect from '../lib/react-redux/connect'
-class Main extends React.Component {
-  render() {
-    return (
-      <div
-        // style={{ color: this.context.themeColor }} // 解耦context
-        style={{ color: this.props.themeColor }}
-      >
-        {this.props.children}
-      </div>
-    )
-  }
+const Main = ({ children, themeColor }) => {
+  return (
+    <div
+      // style={{ color: this.context.themeColor }} // 解耦context
+      style={{ color: themeColor }}
+    >
+      {children}
+    </div>
+  )
 }
+
 export default connect()(Main)
--- src/component/subtitle.js
+++ src/component/subtitle.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import connect from '../lib/react-redux/connect'
-class Sub extends React.Component {
-  render() {
-    return (
-      <div
-        style={{ color: this.props.themeColor }}
-        // onClick={() => {
-        //   // 这样写是没有用的，还是需要通过改变父组件中的state让context起效
-        //   this.context.themeColor = 'green'
-        // }}
-      >
-        {this.props.children}
-      </div>
-    )
-  }
+const Sub = ({ themeColor, children }) => {
+  return (
+    <div
+      style={{ color: themeColor }}
+      // onClick={() => {
+      //   // 这样写是没有用的，还是需要通过改变父组件中的state让context起效
+      //   this.context.themeColor = 'green'
+      // }}
+    >
+      {children}
+    </div>
+  )
 }
 export default connect()(Sub)
